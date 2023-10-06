a790ce0633963886642439e8ba57252ab5725705
Stephon Griffen <56489291+stephon305@users.noreply.github.com>
2023-10-06 01:07:29 +0000

    finale

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,98 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
+
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todos, setTodos] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const [task, setTask] = useState({});
+  const [count, setCount] = useState(2);
+
+  useEffect(() => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/stephon")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTodos(data);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/stephon", {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: { "Content-type": "application/json" },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  }, [todos]);
+
+  const addTodos = (e) => {
+    if (e.key === "Enter") {
+      
+      const newTask = { label: userInput, id: count, done: false };
+      
+     
+      setTask(newTask);
+      
+     
+      setTodos([...todos, newTask]);
+      
+      
+      setUserInput("");
+      
+      
+      setCount(count + 1);
+    }
+  };
+  
+
+  const handleUserInput = (e) => {
+    setUserInput(e.target.value);
+  };
+  
+  const removeTodos = (i) => {
+    const newArray = todos.filter((todo, index) => index !== i);
+    setTodos(newArray);
+  };
+
+  return (
+    <div className="text-center todoList">
+      <input
+        onChange={handleUserInput}
+        value={userInput + " "}
+        onKeyDown={(e) => addTodos(e)}
+        className="my-2"
+      />
+      <ul className="w-25 mx-auto">
+        {/* todos.length ? todos.map() : null */}
+        {todos?.map((todo, index) => (
+          <li className="mx-3" key={index}>
+            {todo.label}
+            <span>
+              <i
+                className="fa-solid fa fa-trash fa-bounce mx-4"
+                onClick={() => removeTodos(index)}
+              ></i>
+            </span>
+            {console.log(todo)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
